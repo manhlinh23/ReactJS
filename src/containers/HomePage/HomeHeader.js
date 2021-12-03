b32a3d5a39d90be1a7ad40fb77a4d970691acace
Mạnh Linh <94213716+manhlinh23@users.noreply.github.com>
2021-12-03 23:22:00 +0700

    #46 header

--- src/containers/HomePage/HomeHeader.js
+++ src/containers/HomePage/HomeHeader.js
@@ -2,50 +2,106 @@ import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import './HomeHeader.scss'
+import { FormattedMessage } from 'react-intl';
+
 class HomeHeader extends Component {
 
     render() {
-
         return (
-            <div className="home-header-container">
-                <div className="home-header-content">
-                    <div className='left-content'>
-                        <i class="fas fa-bars"></i>
-                        <div className='header-logo'></div>
+            <React.Fragment>
+                <div className="home-header-container">
+                    <div className="home-header-content">
+                        <div className='left-content'>
+                            <i class="fas fa-bars"></i>
+                            <div className='header-logo'></div>
+                        </div>
+                        <div className='center-content'>
+                            <div className='child-content'>
+                                <div><b> <FormattedMessage id='homeheader.speciality' /></b></div>
+                                <div className='subtitle'><FormattedMessage id='homeheader.searchdoctor' /></div>
+                            </div>
+
+                            <div className='child-content'>
+                                <div><b> <FormattedMessage id="homeheader.facilities" /></b></div>
+                                <div className='subtitle'><FormattedMessage id="homeheader.choooseclinic" /></div>
+                            </div>
+
+                            <div className='child-content'>
+                                <div><b><FormattedMessage id="homeheader.doctor" /></b></div>
+                                <div className='subtitle'><FormattedMessage id="homeheader.gooddoctor" /></div>
+                            </div>
+
+                            <div className='child-content'>
+                                <div><b><FormattedMessage id="homeheader.packages" /></b></div>
+                                <div className='subtitle'><FormattedMessage id="homeheader.packagescheck" /></div>
+                            </div>
+                        </div>
+
+                        <div className='right-content'>
+
+                            <div className="support">
+                                <i class="fas fa-question"></i>
+                                <FormattedMessage id='homeheader.navsupport' />
+                            </div>
+                            <div className='language-vn'>VN</div>
+                            <div className='language-en'>EN</div>
+                        </div>
+
                     </div>
+                </div>
 
-                    <div className='center-content'>
-                        <div className='child-content'>
-                            <div><b>Chuyên khoa</b></div>
-                            <div className='subtitle'>Tìm bác sĩ theo chuyên khoa</div>
-                        </div>
-
-                        <div className='child-content'>
-                            <div><b>Cơ sở y tế</b></div>
-                            <div className='subtitle'>Chọn bệnh viện phòng khám</div>
-                        </div>
-
-                        <div className='child-content'>
-                            <div><b>Bác sĩ</b></div>
-                            <div className='subtitle'>Chọn bác sĩ giỏi</div>
-                        </div>
-
-                        <div className='child-content'>
-                            <div><b>Gói khám</b></div>
-                            <div className='subtitle'>Khám sức khỏe tổng quát</div>
-                        </div>
+                <div className="home-header-banner">
+                    <div className='title-banner'>
+                        <div className='title1'><FormattedMessage id='homeheader.base' /></div>
+                        <div className='title2'><FormattedMessage id='homeheader.takecare' /></div>
                     </div>
+                    <div className='search'>
+                        <i class="fas fa-search"></i>
+                        <input type='text' placeholder=''></input>
+                    </div>
+                    <div className='download-app'>
+                        <a href='' className='icon-download-app'>
+                            <i class="fab fa-android"></i>
+                        </a>
+                        <a href='' className='icon-download-app'>
+                            <i class="fab fa-app-store-ios"></i>
+                        </a>
 
-                    <div className='right-content'>
-
-                        <div className="support">
-                            <i class="fas fa-question"></i>Hỗ trợ
+                    </div>
+                    <div className='options'>
+                        <div className='bottom-banner'>
+                            <div><i class="fas fa-building"></i></div>
+                            <div className='bottom-banner-sub'><FormattedMessage id='homeheader.spec' /></div>
                         </div>
-                        <div className='flag'>VN</div>
+
+                        <div className='bottom-banner'>
+                            <div><i class="fas fa-building"></i></div>
+                            <div className='bottom-banner-sub'><FormattedMessage id='homeheader.spec' /></div>
+                        </div>
+
+                        <div className='bottom-banner'>
+                            <div><i class="fas fa-building"></i></div>
+                            <div className='bottom-banner-sub'><FormattedMessage id='homeheader.spec' /></div>
+                        </div>
+
+                        <div className='bottom-banner'>
+                            <div><i class="fas fa-building"></i></div>
+                            <div className='bottom-banner-sub'><FormattedMessage id='homeheader.spec' /></div>
+                        </div>
+
+                        <div className='bottom-banner'>
+                            <div><i class="fas fa-building"></i></div>
+                            <div className='bottom-banner-sub'><FormattedMessage id='homeheader.spec' /></div>
+                        </div>
+
+                        <div className='bottom-banner'>
+                            <div><i class="fas fa-building"></i></div>
+                            <div className='bottom-banner-sub'><FormattedMessage id='homeheader.spec' /></div>
+                        </div>
                     </div>
 
                 </div>
-            </div>
+            </React.Fragment>
         );
     }
 
@@ -53,7 +109,8 @@ class HomeHeader extends Component {
 
 const mapStateToProps = state => {
     return {
-        isLoggedIn: state.user.isLoggedIn
+        isLoggedIn: state.user.isLoggedIn,
+        language: state.app.language,
     };
 };
 

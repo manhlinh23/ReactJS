38698e606437aa948e02b035a2f21deadee9b194
Mạnh Linh <94213716+manhlinh23@users.noreply.github.com>
2021-12-05 00:02:45 +0700

    #47 switch languages

--- src/containers/HomePage/HomeHeader.js
+++ src/containers/HomePage/HomeHeader.js
@@ -3,16 +3,26 @@ import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import './HomeHeader.scss'
 import { FormattedMessage } from 'react-intl';
+import { LANGUAGES } from '../../utils'
+
+import { changeLanguageApp } from '../../store/actions'
 
 class HomeHeader extends Component {
 
+
+    changeLanguage = (language) => {
+        this.props.changeLanguageAppRedux(language)
+    }
+
     render() {
+        let language3 = this.props.language10
+        console.log('check language of language 3: ', this.props.language10)
         return (
             <React.Fragment>
                 <div className="home-header-container">
                     <div className="home-header-content">
                         <div className='left-content'>
-                            <i class="fas fa-bars"></i>
+                            <i className="fas fa-bars"></i>
                             <div className='header-logo'></div>
                         </div>
                         <div className='center-content'>
@@ -38,13 +48,20 @@ class HomeHeader extends Component {
                         </div>
 
                         <div className='right-content'>
-
                             <div className="support">
-                                <i class="fas fa-question"></i>
+                                <i className="fas fa-question"></i>
                                 <FormattedMessage id='homeheader.navsupport' />
                             </div>
-                            <div className='language-vn'>VN</div>
-                            <div className='language-en'>EN</div>
+                            <div className={language3 === LANGUAGES.VI ? 'language-vi action' : 'language-vi'}>
+                                <span onClick={() => this.changeLanguage(LANGUAGES.VI)}>
+                                    VN
+                                </span>
+                            </div>
+                            <div className={language3 === LANGUAGES.EN ? 'language-en action' : 'language-en'}>
+                                <span onClick={() => this.changeLanguage(LANGUAGES.EN)}>
+                                    EN
+                                </span>
+                            </div>
                         </div>
 
                     </div>
@@ -56,46 +73,46 @@ class HomeHeader extends Component {
                         <div className='title2'><FormattedMessage id='homeheader.takecare' /></div>
                     </div>
                     <div className='search'>
-                        <i class="fas fa-search"></i>
+                        <i className="fas fa-search"></i>
                         <input type='text' placeholder=''></input>
                     </div>
                     <div className='download-app'>
                         <a href='' className='icon-download-app'>
-                            <i class="fab fa-android"></i>
+                            <i className="fab fa-android"></i>
                         </a>
                         <a href='' className='icon-download-app'>
-                            <i class="fab fa-app-store-ios"></i>
+                            <i className="fab fa-app-store-ios"></i>
                         </a>
 
                     </div>
                     <div className='options'>
                         <div className='bottom-banner'>
-                            <div><i class="fas fa-building"></i></div>
+                            <div><i className="fas fa-building"></i></div>
                             <div className='bottom-banner-sub'><FormattedMessage id='homeheader.spec' /></div>
                         </div>
 
                         <div className='bottom-banner'>
-                            <div><i class="fas fa-building"></i></div>
+                            <div><i className="fas fa-building"></i></div>
                             <div className='bottom-banner-sub'><FormattedMessage id='homeheader.spec' /></div>
                         </div>
 
                         <div className='bottom-banner'>
-                            <div><i class="fas fa-building"></i></div>
+                            <div><i className="fas fa-building"></i></div>
                             <div className='bottom-banner-sub'><FormattedMessage id='homeheader.spec' /></div>
                         </div>
 
                         <div className='bottom-banner'>
-                            <div><i class="fas fa-building"></i></div>
+                            <div><i className="fas fa-building"></i></div>
                             <div className='bottom-banner-sub'><FormattedMessage id='homeheader.spec' /></div>
                         </div>
 
                         <div className='bottom-banner'>
-                            <div><i class="fas fa-building"></i></div>
+                            <div><i className="fas fa-building"></i></div>
                             <div className='bottom-banner-sub'><FormattedMessage id='homeheader.spec' /></div>
                         </div>
 
                         <div className='bottom-banner'>
-                            <div><i class="fas fa-building"></i></div>
+                            <div><i className="fas fa-building"></i></div>
                             <div className='bottom-banner-sub'><FormattedMessage id='homeheader.spec' /></div>
                         </div>
                     </div>
@@ -110,12 +127,13 @@ class HomeHeader extends Component {
 const mapStateToProps = state => {
     return {
         isLoggedIn: state.user.isLoggedIn,
-        language: state.app.language,
+        language10: state.app.language,
     };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
+        changeLanguageAppRedux: (language) => dispatch(changeLanguageApp(language))
     };
 };
 
